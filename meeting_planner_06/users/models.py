01e3445114224cdc6a18e7acf8186123a8aa6c69
Wang <wenbin.c.wang@accenture.com>
2025-01-15 13:37:15 +0900

    initial

---
+++ meeting_planner_06/users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Users(models.Models):
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.user_name}'
+
+    
