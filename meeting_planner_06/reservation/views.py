d287b5a065b4a5710e668aac3a71e5b662679ad2
gobears21-acn <wenbin.c.wang@accenture.com>
2025-01-16 13:04:11 +0900

    latest

--- meeting_planner_06/reservation/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import datetime
-
-
-# Create your views here.
-def welcome(request):
-    return render(request, "website/reservation.html")
